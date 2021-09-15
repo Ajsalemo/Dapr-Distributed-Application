10e27765338b729c56352966eb2dca78c14dc0e2
Anthony Salemo <ajssalemo@gmail.com>
2021-09-15 13:54:42 -0400

    added cssbaseline for consistency, added some more components

--- frontend/src/pages/landing/landing.js
+++ frontend/src/pages/landing/landing.js
@@ -1,6 +1,8 @@
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
+import { Appbar } from "../../components/appbar/appbar";
+import { Fragment } from "react"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,12 +19,15 @@ export const Landing = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+    <Fragment>
+      <Appbar />
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+    </Fragment>
   );
 };
