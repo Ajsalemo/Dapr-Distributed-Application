7eb52a216a3eed21b8466497bc560b9faeb69b5d
Anthony Salemo <ajssalemo@gmail.com>
2021-09-21 13:07:26 -0400

    added .sql files for inserting dummy data

--- backend/database/create_tables.sql
+++ backend/database/create_tables.sql
@@ -1,2 +1,7 @@
 -- Create the initial tables
-CREATE TABLE inventory
+CREATE TABLE inventory(
+    ID SERIAL PRIMARY KEY NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL,
+    image VARCHAR NOT NULL
+);
---
+++ backend/database/insert_values.sql
@@ -0,0 +1,10 @@
+-- Insert initial data
+INSERT INTO inventory(
+    model,
+    price,
+    image
+) VALUES (
+    'Lorem Ipsum',
+    199.99,
+    'https://2.bp.blogspot.com/-mf6pK6W_TSo/ULDla5skUQI/AAAAAAAAACs/J8uxEL8UupE/s1600/10_slippersShopHQP.jpg'
+);
